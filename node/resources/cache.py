d033160fcec719000e2ea158b4d5b16a7ef71165
EvyatarVanunu1 <54750043+EvyatarVanunu1@users.noreply.github.com>
2021-06-16 20:01:15 +0300

    cache route fix

--- node/resources/cache.py
+++ node/resources/cache.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
 from ..models.Cache import cache
+from flask_restful import Resource
+from flask import request, current_app
+
 
 
 class CacheResource(Resource):
@@ -9,7 +12,17 @@ class CacheResource(Resource):
             return {"data": data}, 200, ({"Content-Type": "application/json"})
         return {}, 200, ({"Content-Type": "application/json"})
 
-    def put(self, key, data, expiration_date, time):
-        # TODO: fetch all but key from body
-        cache.set(key, data, expiration_date, time)
+    def get(self):
+        res = {"cache": []}
+        for key, value in cache.items():
+            res[cache].append(value.serialize)
+        return res, 200, ({"Content-Type": "application/json"})
+
+
+    def put(self, key):
+        body = request.get_json() or dict()
+
+        if not body.get("data") or not body.get("expiration_date") or not body.get("time"):
+            return {"msg": "invalid request body. missing data or expiration_date attributes"}, 400
+        cache.set(key, body["data"], body["expiration_date"], body["time"])
         return {"msg": "ok"}, 200, ({"Content-Type": "application/json"})
